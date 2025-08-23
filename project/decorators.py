b5e09106ad13c6daa88d51b338fea63a76be7934
nmtests <optimussentry01@gmail.com>
2025-08-24 04:00:07 +0600

    Update decorators.py

--- project/decorators.py
+++ project/decorators.py
@@ -4,9 +4,6 @@ from flask_jwt_extended import get_jwt_identity
 from flask import jsonify
 
 def admin_required(fn):
-    """
-    শুধুমাত্র অ্যাডমিনদের জন্য রুট সুরক্ষিত করার ডেকোরেটর।
-    """
     @wraps(fn)
     def wrapper(*args, **kwargs):
         current_user = get_jwt_identity()
@@ -16,9 +13,6 @@ def admin_required(fn):
     return wrapper
 
 def super_admin_required(fn):
-    """
-    শুধুমাত্র সুপার অ্যাডমিনদের জন্য রুট সুরক্ষিত করার ডেকোরেটর।
-    """
     @wraps(fn)
     def wrapper(*args, **kwargs):
         current_user = get_jwt_identity()
