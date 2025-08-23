c1496c1d783159ca53c57188768e72670632cfea
nmtests <optimussentry01@gmail.com>
2025-08-24 03:59:23 +0600

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -6,29 +6,24 @@ from flask_migrate import Migrate
 from .config import config
 from .models import db
 
-# Initialize extensions
 cors = CORS()
 jwt = JWTManager()
 migrate = Migrate()
 
 def create_app(config_name='development'):
-    """
-    Application Factory Function
-    """
     app = Flask(__name__,
                 template_folder='../../templates',
                 static_folder='../../static')
-
-    # Load configuration
     app.config.from_object(config[config_name])
 
-    # Initialize extensions with app
     db.init_app(app)
     cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
     jwt.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints
+    from .main.routes import main_bp
+    app.register_blueprint(main_bp)
+
     from .auth.routes import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
@@ -37,8 +32,5 @@ def create_app(config_name='development'):
 
     from .admin.routes import admin_bp
     app.register_blueprint(admin_bp, url_prefix='/api/admin')
-    
-    from .main.routes import main_bp
-    app.register_blueprint(main_bp)
 
     return app
